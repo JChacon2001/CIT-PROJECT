4e2245cc3267f23910b9134f21970eb8d7f9103c
chelcie <ccastro30@my.bcit.ca>
2025-05-02 11:15:34 -0700

    created test card and flip function

--- static/script.js
+++ static/script.js
@@ -1,10 +1,11 @@
-function flipCard(cardElement) {
-    cardElement.classList.toggle('flipped');
-}
-
-
-document.querySelectorAll(".answer").forEach(change => {
-    change.addEventListener('click', function(){
-        flipCard(this)
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+    const card = document.querySelector(".flip-card");
+    card.addEventListener("click", function () {
+      card.querySelector(".flip-card-inner").classList.toggle("flipped");
+    });
+  });
+  
+  function flipCard(card) {
+    card.classList.toggle('flipped');
+  }
+  
