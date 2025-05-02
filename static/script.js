6a22ca9c186ff035a9a766020d08218d662ef653
Jsomera1 <jsomera1@my.bcit.ca>
2025-05-02 11:16:14 -0700

    javascript and css

--- static/script.js
+++ static/script.js
@@ -1,5 +1,14 @@
 function flipCard(cardElement) {
     cardElement.classList.toggle('flipped');
+    if(cardElement.classList.contains('flipped')) {
+        cardElement.classList.add('unflip');
+        setTimeout(function(){
+          cardElement.classList.remove('flipped', 'unflip');
+        }, 500);
+      }
+      else { 
+        cardElement.classList.add("flipped");
+      }
 }
 
 
