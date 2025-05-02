ea7a5a403a7a8045d517dcc53b57351e0a4c79fb
Jian <jsomera1@my.bcit.ca>
2025-05-01 21:26:47 -0700

    more DOM testing

--- static/script.js
+++ static/script.js
@@ -1,5 +1,18 @@
-document.querySelectorAll(".cardeck").forEach(flip => {
+document.querySelectorAll(".carddeck").forEach(flip => {
+    
     flip.addEventListener('click', function(){
+
+        if (this.innerHTMl == "flipped"){
+            this.innerHTML = "unflipped"
+        }
+        else{
+            this.innerHTML = "flipped" 
+            this.innerHTML += `<button id="flip"> flip </button>`
+        }
+        
+
+        
+        
         
     })
 })
