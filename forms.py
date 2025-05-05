c0822fa68e572149f8a1d67984dccbe43a8b78b3
JChacon2001 <JORGECHACON14@OUTLOOK.COM>
2025-05-04 17:43:16 -0700

    edited forms for flask

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+
+class DeckForm(FlaskForm):
+    name = StringField("Deck Name")
+    description = TextAreaField("Description")
+    submit = SubmitField("Save Deck")
+    
+class CardForm(FlaskForm):
+    deck = SelectField('Deck', coerce=int)
+    question = TextAreaField("Question")
+    answer = TextAreaField("Answer")
+    submit = SubmitField("Save Card")
