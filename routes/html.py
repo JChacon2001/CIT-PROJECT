5c429137930496cbf143928fabed992bd1978681
JChacon2001 <JORGECHACON14@OUTLOOK.COM>
2025-05-15 14:10:54 -0700

    fixed edit form

--- routes/html.py
+++ routes/html.py
@@ -75,7 +75,7 @@ def deck_form():
 @html_bp.route('/card/new', methods=['GET', 'POST'])
 def create_card():
     form = CardForm()
-    stmt = db.select(Deck).order_by(Deck.name)
+    stmt = db.select(Deck   ).order_by(Deck.name)
     decks = db.session.execute(stmt).scalars()
     form.deck.choices = []
     for deck in decks:
@@ -108,6 +108,8 @@ def edit_deck(id):
     stmt = db.select(Deck).where(Deck.id == id)
     deck = db.session.execute(stmt).scalar()
     form = DeckForm(obj=deck)
+    if request.method == "GET" and deck.category:
+        form.category.data = deck.category.name
     if form.validate_on_submit():
         deck.name = form.name.data
         deck.description   = form.description.data
@@ -117,6 +119,7 @@ def edit_deck(id):
         if category is None:
             category = Category(name=cat_name)
             db.session.add(category)
+        deck.category = category
         db.session.commit()
         return redirect(url_for('html.decks', id=id))
     return render_template('edit_deck.html', form=form, deck=deck)
